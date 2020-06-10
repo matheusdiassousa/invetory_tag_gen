f7ffb44f983747f61a3eb04222f0aa06972fe8c0
Matheus <matheus_diassousa@hotmail.com>
2020-06-10 17:11:19 -0300

    v3

--- qr_functions.py
+++ qr_functions.py
@@ -1,9 +1,11 @@
-import pyqrcode
 from pyqrcode import QRCode
 import matplotlib.pyplot as plt
 import matplotlib.image as pltimg
 import os
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
+
+
+
 
 #bibliotecas instaladas -> pyqrcode, pypng, pandas, Pillow e matplotlib
 #deletar a imagem depois de visualizada set -> delete_file = True
@@ -11,7 +13,7 @@ from PIL import Image
 #delete_temp_files = True;
 
 
-def adhesive_tag_75x25(data, file_name, delete_temp_files):
+def adhesive_tag_75x25(data, file_name, delete_temp_files, data_pd, data_model, data_item):
     qrcode = QRCode(data);
     qrcode.png('images/qrcode_file.png', scale=6);
 
@@ -21,20 +23,36 @@ def adhesive_tag_75x25(data, file_name, delete_temp_files):
     qrcode_img_resized = Image.open('images/qrcode_file_resized.png');
 
     eld_logo = Image.open('images/logo_eldorado_597x160.png');
-    eld_logo.thumbnail((190, 190));
+    eld_logo.thumbnail((250, 250));
     eld_logo.save('images/eld_logo_resized.png');
     eld_logo_resized = Image.open('images/eld_logo_resized.png');
 
     background_75x25 = Image.open('images/75x25_background.png');
     img_mounting = background_75x25.copy();
 
-    eld_logo_pos = ( (int(background_75x25.width/2) - int(eld_logo_resized.width/2)) , (0) );
+    #eld_logo_pos = ( (int(background_75x25.width/2) - int(eld_logo_resized.width/2) - 40) , (15) );
+    eld_logo_pos = ( (40) , (40) );
     qrcode_img_pos = ( (int(background_75x25.width) - int(qrcode_img_resized.width) - 25) , ( int(background_75x25.height/2) - int(qrcode_img_resized.height/2) ));
     img_mounting.paste(eld_logo_resized, eld_logo_pos, eld_logo_resized);
     img_mounting.paste(qrcode_img_resized, qrcode_img_pos);
+    
 
 
     img_mounting.save('images/' + file_name + '.png');
+
+    qr_img = Image.open('images/' + file_name + '.png');
+    qr_img.thumbnail((595, 595));
+    
+    t1 = ImageDraw.Draw(qr_img);
+    textFont = ImageFont.truetype('fonts/ARIALNB.TTF', 22);
+    text2Font = ImageFont.truetype('fonts/ARIALNB.TTF', 30);
+    t1.text( (5,100), data_pd, font = textFont, fill=(0,0,0));
+    t1.text( (5,130), 'PN: ' + data_model, font = textFont, fill=(0,0,0));
+    t1.text( (5,150), 'Item: ' + data_item, font = text2Font, fill=(0,0,0));
+
+    qr_img.save('images/' + file_name + '.png');
+    qr_img = Image.open('images/' + file_name + '.png').convert('LA');
+    qr_img.save('images/' + file_name + '.png');
 
 
 
--- qr_generator.py
+++ qr_generator.py
@@ -1,3 +1,23 @@
 from qr_functions import *
+import pandas as pd
 
-adhesive_tag_75x25('texto qualquer com 123123', 'test1', True);
+#optional dependecy of pandas installed > pip install xlrd
+
+data = pd.read_excel('input_data/Eldorado_Inventory_09062020.xlsm', skiprows=6);
+col_names = data.columns.values.tolist();
+noclass_values = data[col_names[1]].values;
+#print(noclass_values);
+#print(col_names[1]);
+
+#print(data.loc[ 8 ,['Item', 'Product Description']]);
+
+numb_item = 25;
+
+data_item = data.loc[numb_item , 'Item'];
+data_pd = data.loc[numb_item, 'Product Description'];
+data_model = data.loc[numb_item, 'Product Model'];
+
+filename = str(data_item) + '_' + data_model;
+
+#adhesive_tag_75x25(str(data_item) + '/' + data_model + '/' + data_pd, filename, True, str(data_pd), str(data_model), str(data_item));
+adhesive_tag_75x25(str('19801ade-3f7e-4195-9e25-9b112021eacd'), filename, True, str(data_pd), str(data_model), str(data_item));
