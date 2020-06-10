9d69b0a6af5e01e3fb793234d9a0dc561df8d107
Matheus <matheus_diassousa@hotmail.com>
2020-06-09 23:55:41 -0300

    v2

--- qr_generator.py
+++ qr_generator.py
@@ -8,42 +8,48 @@ from PIL import Image
 #bibliotecas instaladas -> pyqrcode, pypng, pandas, Pillow e matplotlib
 #deletar a imagem depois de visualizada set -> delete_file = True
 
-delete_file = True;
+delete_temp_files = True;
 
-qrcode = QRCode('numero do item');
+qrcode = QRCode('arroz com feijão #12115132523523523526299@@..//');
 qrcode.png('images/qrcode_file.png', scale=6);
+
 
 #img = pltimg.imread('images/qrcode_file.png');
 #img_plot = plt.imshow(img, cmap='gray');
 #plt.show()
 
-qrcode_img = Image.open('images/qrcode_file.png');
+def adhesive_tag_75x25():
+    qrcode_img = Image.open('images/qrcode_file.png');
+    qrcode_img.thumbnail((222,222));
+    qrcode_img.save('images/qrcode_file_resized.png');
+    qrcode_img_resized = Image.open('images/qrcode_file_resized.png');
 
-eld_logo = Image.open('images/logo_eldorado_597x160.png');
-eld_logo.thumbnail((190, 190));
-eld_logo.save('images/eld_logo_resized.png');
-eld_logo_resized = Image.open('images/eld_logo_resized.png');
+    eld_logo = Image.open('images/logo_eldorado_597x160.png');
+    eld_logo.thumbnail((190, 190));
+    eld_logo.save('images/eld_logo_resized.png');
+    eld_logo_resized = Image.open('images/eld_logo_resized.png');
 
-background_75x25 = Image.open('images/75x25_background.png');
-img_mounting = background_75x25.copy();
+    background_75x25 = Image.open('images/75x25_background.png');
+    img_mounting = background_75x25.copy();
 
-eld_logo_pos = ( (int(background_75x25.width/2) - int(eld_logo_resized.width/2)) , (0) );
-qrcode_img_pos = ( (int(background_75x25.width) - int(qrcode_img.width) - 25) , ( int(background_75x25.height/2) - int(qrcode_img.height/2) ));
-img_mounting.paste(eld_logo_resized, eld_logo_pos, eld_logo_resized);
-img_mounting.paste(qrcode_img, qrcode_img_pos);
-
-
-img_mounting.save('images/mounted_img_75x25.png');
+    eld_logo_pos = ( (int(background_75x25.width/2) - int(eld_logo_resized.width/2)) , (0) );
+    qrcode_img_pos = ( (int(background_75x25.width) - int(qrcode_img_resized.width) - 25) , ( int(background_75x25.height/2) - int(qrcode_img_resized.height/2) ));
+    img_mounting.paste(eld_logo_resized, eld_logo_pos, eld_logo_resized);
+    img_mounting.paste(qrcode_img_resized, qrcode_img_pos);
 
 
-
-
-#imgpil.show()
-if(delete_file == True):
-    os.remove('images/eld_logo_resized.png');
+    img_mounting.save('images/mounted_img_75x25.png');
 
 
 
+
+    #imgpil.show()
+    if(delete_temp_files == True):
+        os.remove('images/eld_logo_resized.png');
+        os.remove('images/qrcode_file_resized.png');
+
+
+adhesive_tag_75x25();
 
 
 '''PILLOW - Changing Image Type
