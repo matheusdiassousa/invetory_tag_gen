894676ac12f761ee7a0d5c0743c4b35759abb951
Matheus <matheus_diassousa@hotmail.com>
2020-06-24 15:27:11 -0300

    inventory summary problem

--- temp.py
+++ temp.py
@@ -12,88 +12,23 @@ data_inventory = pd.read_excel(excel_file, sheet_name='inventory_form', skiprows
 data_withdrawal = pd.read_excel(excel_file, sheet_name='withdrawal_form', skiprows=6);
 
 col_inventory = data_inventory.columns.values.tolist();
-#print(col_inventory);
+print(col_inventory);
 col_withdrawal = data_withdrawal.columns.values.tolist();
-#print(col_withdrawal);
+print(col_withdrawal);
 
 '''-----------------------'''
 inv_item = [];
-w_PN = [];
-w_genuse = [];
-w_pj1 = [];
-w_pj2 = [];
-w_pj3 = [];
-w_pj4 = [];
-
 inventory_Item = data_inventory[col_inventory[1]].values;
 inventory_PN = data_inventory[col_inventory[5]].values;
-
 withdrawal_PN = data_withdrawal[col_withdrawal[0]].values;
-withdrawal_genuse = data_withdrawal[col_withdrawal[6]].values;
-withdrawal_pj1 = data_withdrawal[col_withdrawal[7]].values;
-withdrawal_pj2 = data_withdrawal[col_withdrawal[8]].values;
-withdrawal_pj3 = data_withdrawal[col_withdrawal[9]].values;
-withdrawal_pj4 = data_withdrawal[col_withdrawal[10]].values;
-
-#print(withdrawal_PN);
-#print(inventory_Item);
-
+print(withdrawal_PN);
+print(inventory_Item);
 for item in inventory_Item:
     if(str(data_inventory.loc[item, 'Product Description']) != 'nan'):
         inv_item.append(inventory_Item[item]);
+
 inventory_Item = inv_item;
-
-for item in withdrawal_PN:
-    w_PN.append(item);
-withdrawal_PN = w_PN;
-
-for item in withdrawal_genuse:
-    if(item == 'nan'):
-        w_genuse.append(0);
-    else:
-        w_genuse.append(item);
-withdrawal_genuse = w_genuse;
-
-for item in withdrawal_pj1:
-    if(item == 'nan'):
-        w_pj1.append(0);
-    else:
-        w_pj1.append(item);    
-    
-withdrawal_pj1 = w_pj1;
-
-for item in withdrawal_pj2:
-    if(item == 'nan'):
-        w_pj2.append(0);
-    else:
-        w_pj2.append(item);
-
-withdrawal_pj2 = w_pj2;
-
-for item in withdrawal_pj3:
-    if(item == 'nan'):
-        w_pj3.append(0);
-    else:
-        w_pj3.append(item);
-
-withdrawal_pj3 = w_pj3;
-
-for item in withdrawal_pj4:
-    if(item == 'nan'):
-        w_pj4.append(0);
-    else:
-        w_pj4.append(item);
-
-
-withdrawal_pj4 = w_pj4;
-
-#i=0;
-#for item in withdrawal_PN:
-#    print(withdrawal_PN[i], ' , ', withdrawal_genuse[i], ' , ', withdrawal_pj1[i], ' , ', withdrawal_pj2[i], ' , ', withdrawal_pj3[i], ' , ', withdrawal_pj4[i])
-#    i += 1;
-
-
-
+'''-----------------------'''
 
 sum_descr = ['']*len(inventory_Item);
 sum_pn = ['']*len(inventory_Item);
@@ -122,26 +57,25 @@ for item in inventory_Item:
     i = 0;
     for with_item_PN in withdrawal_PN:
 
-        if ( str(with_item_PN[i]) == str(sum_pn[item]) ):
-
+        if ( str(with_item_PN) == str(sum_pn[item]) ):
             try:                
-                sum_flag_gen[item] += int(withdrawal_genuse[i]);
+                sum_flag_gen[item] += int(data_withdrawal.loc[i, 'General Use']);
             except:
                 sum_flag_gen[item] += 0;
             try:                
-                sum_flag_pj1[item] += int(withdrawal_pj1[i]);
+                sum_flag_pj1[item] += int(data_withdrawal.loc[i, 'Project_1']);
             except:
                 sum_flag_pj1[item] += 0;
             try:                
-                sum_flag_pj2[item] += int(withdrawal_pj2[i]);
+                sum_flag_pj2[item] += int(data_withdrawal.loc[i, 'Project_2']);
             except:
                 sum_flag_pj2[item] += 0;
             try:                
-                sum_flag_pj3[item] += int(withdrawal_pj3[i]);
+                sum_flag_pj3[item] += int(data_withdrawal.loc[i, 'Project_3']);
             except:
                 sum_flag_pj3[item] += 0;
             try:                
-                sum_flag_pj4[item] += int(withdrawal_pj4[i]);
+                sum_flag_pj4[item] += int(data_withdrawal.loc[i, 'Project_4']);
             except:
                 sum_flag_pj4[item] += 0;                                 
 
@@ -162,16 +96,16 @@ for item in inventory_Item:
             i+=1;
 
         else:
-            sum_flag_gen[item] += 0;
-            sum_flag_pj1[item] += 0;
-            sum_flag_pj2[item] += 0;
-            sum_flag_pj3[item] += 0;
-            sum_flag_pj4[item] += 0;
+            sum_flag_gen[item] = 0;
+            sum_flag_pj1[item] = 0;
+            sum_flag_pj2[item] = 0;
+            sum_flag_pj3[item] = 0;
+            sum_flag_pj4[item] = 0;
             sum_fqty[item] = data_inventory.loc[item, 'Qty'];
 
 #i = 0;
-for item in inventory_Item:
-    print(sum_descr[item], sum_pn[item], sum_unit[item], sum_tqty[item], sum_fqty[item], sum_flag_gen[item], sum_flag_pj1[item], sum_flag_pj2[item], sum_flag_pj3[item], sum_flag_pj4[item]);
+#for item in inventory_Item:
+#    print(sum_descr[item], sum_pn[item], sum_unit[item], sum_tqty[item], sum_fqty[item], sum_flag_gen[item], sum_flag_pj1[item], sum_flag_pj2[item], sum_flag_pj3[item], sum_flag_pj4[item]);
 #    i+=1;
     
 book = load_workbook(excel_file);
@@ -198,3 +132,4 @@ df = pd.DataFrame({
 
 df.to_excel(writer, sheet_name = 'summary_form', index = False, header=False, startrow=7);
 writer.save();
+
